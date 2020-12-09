e704f25ecb465c080cd9ccc4975ec60b0d7a5db6
Steve <stevestephenas7@gmail.com>
2020-12-09 22:23:54 +0530

    Started

---
+++ src/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <h2>Login Component</h2>
+    </div>
+  );
+}
+
+export default Login;
