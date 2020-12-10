b0b0c5997985db20dcfbdf6a579f141812931e87
Steve <stevestephenas7@gmail.com>
2020-12-10 09:05:47 +0530

    Login page with logo

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./Login.css";
+import { loginUrl } from "./spotify";
 
 function Login() {
   return (
     <div className="login">
-      <h2>Login Component</h2>
+      <img
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+      <a href={loginUrl}>LOGIN TO SPOTIFY</a>
     </div>
   );
 }
