736ba2a72ae118c01f8597281f4c83d2bbd88491
Steve <stevestephenas7@gmail.com>
2020-12-10 10:36:21 +0530

    Checked user with Datalayer

--- src/App.js
+++ src/App.js
@@ -2,23 +2,35 @@ import { React, useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
+  const [{ user }, dispatch] = useDataLayerValue();
+
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
     const _token = hash.access_token;
 
     if (_token) {
-      setToken(_token);
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+      });
     }
-    console.log(" Token Url point : ", token);
   }, []);
+  console.log(" user is - ", user);
 
   return (
     //BEM naming concept
-    <div className="app">{!token ? <Login /> : <h2>Logged in</h2>}</div>
+    <div className="app">{!user ? <Login /> : <Player />}</div>
   );
 }
 
