282473a35c16b9a8292f722e66a58615dfbd4dc1
Steve <stevestephenas7@gmail.com>
2020-12-10 09:06:57 +0530

    Retrieving the user token

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import { React } from "react";
+import { React, useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+    console.log(" Token Url point : ", token);
+  }, []);
+
   return (
     //BEM naming concept
-    <div className="app">
-      <Login />
-    </div>
+    <div className="app">{!token ? <Login /> : <h2>Logged in</h2>}</div>
   );
 }
 
