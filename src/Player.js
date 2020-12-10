a1def532b0752e61ebfeeeca15fd6c003b689f3a
Steve <stevestephenas7@gmail.com>
2020-12-10 22:42:37 +0530

    Completed sidebar component

--- src/Player.js
+++ src/Player.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./Player.css";
 import Sidebar from "./Sidebar";
+import Body from "./Body";
+import Footer from "./Footer";
 
 function Player({ spotify }) {
   return (
     <div className="player">
-      <Sidebar />
+      <div className="player__body">
+        <Sidebar />
+        <Body />
+      </div>
+      <Footer spotify={spotify} />
     </div>
   );
 }
