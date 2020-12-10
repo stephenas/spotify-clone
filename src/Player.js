1b8e479d8b8f71e830345a94681ed03ba991e117
Steve <stevestephenas7@gmail.com>
2020-12-10 09:28:24 +0530

    Initial Commit

---
+++ src/Player.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Player.css";
+
+function Player() {
+  return (
+    <div className="player">
+      <h2>Welcome to Spotify</h2>
+    </div>
+  );
+}
+
+export default Player;
